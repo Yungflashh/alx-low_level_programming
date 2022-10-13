2abe24e8283e7dfa836e3ef5033129d4dc9a5abb
david <streaktv8@gmail.com>
2022-10-13 16:25:45 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+
+
+
 /**
-   * main - Entry point
-    * Return: Always 0 (Success)
-     */
-
-
-
+ * main- Entry point
+ * @void: parameter
+ * Return: 1 (success)
+ **/
 int main(void)
 
-	    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-	        return (1);
+{
+	fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+	return (1);
 
 }
