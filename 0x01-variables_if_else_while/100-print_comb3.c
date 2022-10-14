74a803f4b0d93d77245afff7ccc22860e58de8d1
David <kayskidadenusi@gmail.com>
2022-10-14 15:58:35 -0700

    msg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,5 @@
-/*
-** File: 10-print_comb2.c
-** Auth: Brennan D Baraban
+ /*
+**File: 100-print_comb3.c
 */
 
 
@@ -8,22 +7,21 @@
 
 
 /**
-** main - Prints the numbers from 00 to 99, numbers separated by
-**a comma followed by a space, in ascending order.
+**main - Prints all possible combinations of two different digits,
+**in ascending order, separated by a comma followed by a space.
 **Return: Always 0.
 */
 
 int main(void)
 
 {
-
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 
 	{
 
-		for (digit2 = 0; digit2 < 10; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 
 		{
 
@@ -31,7 +29,7 @@ int main(void)
 
 			putchar((digit2 % 10) + '0');
 
-			if (digit1 == 9 && digit2 == 9)
+			if (digit1 == 8 && digit2 == 9)
 
 				continue;
 
@@ -48,5 +46,3 @@ int main(void)
 	return (0);
 
 }
-
-
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,4 @@
-/**
+/*
 ** File: 101-print_comb4.c
 ** Auth: Brennan D Baraban
  */
@@ -7,7 +7,7 @@
 #include <stdio.h>
 
 
-/**
+/*
 ** main - Prints all possible combinations of three different digits,
 **in ascending order, separated by a comma followed by a space.
 **Return: Always 0.
@@ -19,9 +19,8 @@ int main(void)
 	int digit1, digit2, digit3;
 
 	for (digit1 = 0; digit1 < 8; digit1++)
-
 	{
-		
+
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 
 		{
@@ -32,42 +31,24 @@ int main(void)
 
 				putchar((digit1 % 10) + '0');
 
-                
 				putchar((digit2 % 10) + '0');
 
-                
 				putchar((digit3 % 10) + '0');
 
-
-                
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 
-                    
 					continue;
 
-                
-
-                
 				putchar(',');
 
-                
 				putchar(' ');
 
-            
 			}
 
-        
 		}
-
-    
 	}
 
-
-    
 	putchar('\n');
 
-
-    
 	return (0);
-
 }
