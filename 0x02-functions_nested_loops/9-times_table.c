8ba3d7a8b902ab2816fe4bb2805bc58a44aea545
Beth <kayskidadenusi@gmail.com>
2022-10-18 15:49:32 -0700

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,14 +17,23 @@ void times_table(void)
 	for (row = 0; row <= 9; row++)
 	{
 		for (column = 0; column <= 9; column++)
+		
 		{
+			
 			product = row * column;
+			
 			tens = product / 10;
+			
 			if (column == 0)
+			
 			{
+			
 				_putchar('0');
+			
 			}
+			
 			else if (product < 10)
+			
 			{
 				_putchar(',');
 				_putchar(' ');
