a14560bd7365c3e8ed7d6edc585f97aaa43b9538
Beth <kayskidadenusi@gmail.com>
2022-10-18 15:35:56 -0700

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /**
-
- * numLength - returns the lenth of string
-
- * @num : operand number
-
- * Return: number of digits
-
- */
+*numLength - returns the lenth of string
+*@num : operand number
+*Return: number of digits
+*/
 
 
 int numLength(int num)
