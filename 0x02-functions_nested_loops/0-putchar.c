a55e43a86abbfab34b5562f2080e8a702bd57bd5
Beth <kayskidadenusi@gmail.com>
2022-10-18 12:41:20 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "_putchar"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+** main - Entry point
+**
+** Return: Always 0 (Success)
+*/
 int main(void)
 {
 	char *sh = "_putchar";
 
 	while (*sh)
+
 	{
 		_putchar(*sh);
 		sh++;
 	}
+
 	_putchar('\n');
 
 	return (0);
