db58ba8fe483a626ba3124baeb0b6912f17dbc6b
Beth <kayskidadenusi@gmail.com>
2022-10-18 16:13:21 -0700

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,8 @@
 *Return: Always 0.
 */
 void print_to_98(int n)
-	if (n >=98)
+n=0;
+if (n >=98)
 {
 	while (n > 98)
 		printf ("%d, ", n--);
@@ -17,5 +18,5 @@ else
 {
 	while (n < 98)
 		printf ("%d, ", n++);
-	printf("%d\n", n)'
+	printf("%d\n", n);
 }
