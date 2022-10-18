4db6c46f1ee15c48be4edfad9f29b5823f10e267
Beth <kayskidadenusi@gmail.com>
2022-10-18 16:09:41 -0700

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,24 +5,17 @@
 *main - check the code for ALX School students.
 *Return: Always 0.
 */
-
-int main(void)
-
+void print_to_98(int n)
+	if (n >=98)
 {
-	times_table(3);
+	while (n > 98)
+		printf ("%d, ", n--);
+	printf("%d\n", n);
+}
 
-	_putchar('\n');
-
-	times_table(5);
-
-	_putchar('\n');
-
-	times_table(98);
-
-	_putchar('\n');
-
-	times_table(12);
-
-	return (0);
-
+else 
+{
+	while (n < 98)
+		printf ("%d, ", n++);
+	printf("%d\n", n)'
 }
