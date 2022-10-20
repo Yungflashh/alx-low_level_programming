59787e2c4a0c10e87d9ab9f9caf92b1734b7d878
David <streaktv8@gmail.com>
2022-10-20 12:17:42 -0700

    initial commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
 *main - tests function that prints if integer is positive or negative
 *Return: 0
@@ -8,7 +7,6 @@
 
 
 int main(void)
-
 {
 	int i;
 
@@ -17,5 +15,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-
 }
