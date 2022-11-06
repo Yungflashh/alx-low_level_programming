41840b1929d5c63865c84a50fe445d1630059ae6
DAVID <streaktv8@gmail.com>
2022-11-06 08:53:45 -0800

    initial commit

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,3 +1,18 @@
 #include "main.h"
-void _puts(char *s)
-{}
+/**
+*_puts - prints a string, followed by a new line, to stdout
+*@str: string to print
+*/
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+
+	{
+
+		_putchar(*str++);
+
+	}
+
+	_putchar('\n');
+}
